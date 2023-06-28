04cab5e9d8f0ab95373d6f127bebd2d636de9ff6
rohmatsetiawanmta <rohmatsetiawanmta@gmail.com>
2023-06-28 22:20:23 +0700

    Slicing Checkout and Success Checkout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="{{route('welcome')}}">
                 <h3><strong>campsite.</strong></h3>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
